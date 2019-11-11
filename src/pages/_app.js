0df0d401ebb846e2660a28da6904dd5ce53aeb63
Kendall Strautman <kendallstrautman@gmail.com>
2019-11-11 14:55:43 -0800

    adds dotenv, hides Tina in prod

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,10 +10,15 @@ class MyApp extends App {
     const client = new GitClient('http://localhost:3001/___tina')
     this.cms.registerApi('git', client)
   }
+  options = {
+      sidebar: {
+        hidden: process.env.NODE_ENV === "production"
+      }
+  }
   render() {
     const { Component, pageProps } = this.props
     return (
-      <Tina cms={this.cms}>
+      <Tina cms={this.cms} {...this.options.sidebar}>
         <Component {...pageProps} />
       </Tina>
     )
