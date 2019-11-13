125c32796488e0f4aadd99ac96c6399d548e43d6
Kendall Strautman <kendallstrautman@gmail.com>
2019-11-13 14:03:34 -0800

    feat: adds styled-jsx to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,47 @@
 import Header from "./Header";
 import Meta from './Meta'
-import layoutStyles from "../styles/components/layout.scss";
 
 export default function Layout(props) {
   return (
     <section
-      className={`${layoutStyles.layout} ${
-        props.pathname == "info" &&
-        layoutStyles.info_page}`
-      }
-      style={{
-        backgroundColor: `${props.bgColor && props.bgColor}`,
-        color: `${props.pathname == "info" && 'white'}`
-      }}
-    >
-      <Meta />
-      <Header siteTitle={props.siteTitle} />
-      <div className={layoutStyles.content}>
-        {props.children}
-      </div>
-    </section>
+    className={`layout ${
+      props.pathname == "info" &&
+      "info_page"}`
+    }
+    style={{
+      backgroundColor: `${props.bgColor && props.bgColor}`,
+      color: `${props.pathname == "info" && 'white'}`
+    }}
+  >
+    <Meta />
+    <Header siteTitle={props.siteTitle} />
+    <div className="content">{props.children}</div>
+    <style jsx>
+      {`
+        .layout {
+          overflow-x: hidden;
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+        .layout .info_page {
+          color: #ebebeb;
+        }
+        .content {
+          flex-grow: 1;
+        }
+        @media (min-width: 768px) {
+          .layout {
+            display: block;
+          }
+          .content {
+            flex-grow: none;
+            width: 70vw;
+            margin-left: 30vw;
+          }
+        }
+      `}
+    </style>
+  </section>
   );
 }
