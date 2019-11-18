bffe6ffaf6a855eab7f3d59e547faca1aec8c6d4
Kendall Strautman <kendallstrautman@gmail.com>
2019-11-18 11:55:35 -0800

    other: moves blog list to index getinitialprops

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,34 +1,7 @@
 import Link from "next/link";
-import matter from "gray-matter";
-import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
-const BlogList = () => {
-
-  function getPosts() {
-    //get posts & context from folder
-    const posts = (ctx => {
-      const keys = ctx.keys();
-      const values = keys.map(ctx);
-      const data = keys.map((key, index) => {
-        // Create slug from filename
-        const slug = key
-          .replace(/^.*[\\\/]/, "")
-          .split(".")
-          .slice(0, -1)
-          .join(".");
-        const value = values[index];
-        // Parse yaml metadata in document
-        const document = matter(value.default);
-        return {
-          document,
-          slug
-        };
-      });
-      return data;
-    })(require.context("../posts", true, /\.md$/));
-    return posts;
-  }
+const BlogList = (props) => {
 
   function truncateSummary(content) {
     return content.slice(0, 200).trimEnd();
@@ -39,8 +12,7 @@ const BlogList = () => {
     return date.toDateString().slice(4);
   }
 
-  //store the posts in state with hook
-  const [posts] = useState(getPosts());
+  const posts = props.allBlogs
 
   return (
     <>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { useCMS, useCMSForm, useWatchFormValues } from 'react-tinacms'
+import matter from 'gray-matter'
 
 import Layout from "../components/Layout";
 import BlogList from "../components/BlogList";
@@ -67,7 +68,7 @@ const Index = (props) => {
   return (
     <Layout pathname="/" siteTitle={data.title} siteDescription={data.description}>
       <section>
-        <BlogList />
+        <BlogList allBlogs={props.allBlogs} />
       </section>
       </Layout>
   );
@@ -77,9 +78,31 @@ export default Index;
 
 Index.getInitialProps = async function() {
   const content = await import(`../data/config.json`)
+     // get all blog data for list
+     const posts = (context => {
+      const keys = context.keys();
+      const values = keys.map(context);
+      const data = keys.map((key, index) => {
+        // Create slug from filename
+        const slug = key
+          .replace(/^.*[\\\/]/, "")
+          .split(".")
+          .slice(0, -1)
+          .join(".");
+        const value = values[index];
+        // Parse yaml metadata & markdownbody in document
+        const document = matter(value.default);
+        return {
+          document,
+          slug
+        };
+      });
+      return data;
+    })(require.context("../posts", true, /\.md$/));
 
   return {
     fileRelativePath: `src/data/config.json`,
+    allBlogs: posts,
     ...content
   }
 }
